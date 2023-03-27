32e8130d4114e74e27bf8e044d90a0f1e4a752f3
piraveena <piraveena18@gmail.com>
2023-03-27 21:40:01 +0530

    initial commit

---
+++ src/main/java/com/example/demo/SimpleController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+    @GetMapping("/")
+    String hello() {
+        return "Hello!";
+    }
+
+}
